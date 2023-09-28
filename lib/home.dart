1c95db2c4667983cc31e28303bceaad63d124fe8
aacfahim <aacfahim@gmail.com>
2023-09-28 21:42:58 +0600

    dark and light mode

--- lib/home.dart
+++ lib/home.dart
@@ -1,43 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class Home extends StatefulWidget {
-  Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  bool _switchValue = false;
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [
-        Switch.adaptive(
-          value: _switchValue,
-          onChanged: (value) {
-            _switchValue = value;
-          },
-        )
-      ]),
-      body: Column(children: [
-        Text("Hello"),
-        ElevatedButton(
-            onPressed: () {
-              Get.changeTheme(ThemeData.dark());
-            },
-            child: Text("Dark")),
-        ElevatedButton(
-            onPressed: () {
-              Get.changeTheme(ThemeData.light());
-            },
-            child: Text("Light")),
-      ]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+      body: ListView(
+        children: [
+          TextButton(
+              onPressed: () {
+                Get.changeTheme(ThemeData.dark());
+              },
+              child: Text("Dark")),
+          TextButton(
+              onPressed: () {
+                Get.changeTheme(ThemeData.light());
+              },
+              child: Text("Light")),
+        ],
       ),
     );
   }
